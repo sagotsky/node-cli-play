1ff4e860834729147702ad8b76d7441d9c63e67c
Jon Sagotsky <sagotsky@gmail.com>
2016-02-13 15:41:33 -0500

    Trying out yargs
    Commands look great.  I can't get all the option parsing options to work
    yet though.  Defaults show up sometimes.  I'd rather not deal with
    checking if they're in my args hash and just have the default work.  If
    it doesn't, will try commander.

--- bin/spotifyr.js
+++ bin/spotifyr.js
@@ -7,32 +7,55 @@ require('app-module-path').addPath(__dirname + '/../lib')
 var EsMenu = require('esmenu'),
   Spotifyr = require('spotifyr')
 
-function help() {
-  console.log('#TODO: write some help message.  hopefully yargs can provide them')
+var client = new Spotifyr(EsMenu)
+
+var argv = require('yargs')
+  .usage('spotifyr <command>')
+  .require(1)
+  .strict(true)
+
+  .option('v', {
+    alias: 'verbose', 
+    describe: 'show status after track change', 
+    default: false,
+    requiresArg: false,
+    type: 'boolean'
+  })
+
+  .option('q', {
+    alias: 'query', 
+    describe: 'Query string for search commands.  If blank, you will be prompted.', 
+    type: 'string',
+    requiresArg: true,
+    default: ''
+  })
+
+  .command('next'       , 'Play next song'     , player_cmd)
+  .command('previous'   , 'Play previous song' , player_cmd)
+  .command('play_pause' , 'Toggle play/pause'  , player_cmd)
+  .command('pause'      , 'Pause playback'     , player_cmd)
+  .command('stop'       , 'Stop playback'      , player_cmd)
+  .command('play'       , 'Play'               , player_cmd)
+  .command('status'     , 'Show current song'  , player_cmd)
+
+  .command('search', 'Search spotify for any type of media and play it.', (yargs) => {
+    search_cmd(yargs)
+  })
+
+  .argv
+
+function player_cmd(yargs) {
+  //init client with special opts here?
+  let args = yargs.argv
+  let cmd = args._[0]
+  client[cmd]()
+  console.log(args)
+  if (args['verbose']) {
+    client.metadata
+  }
 }
 
-let argv = require('yargs').argv
-let cmd = argv._[0]
-let client = new Spotifyr(EsMenu)
-// console.log(argv)
-//omg we need better arg parsing
-
-switch (cmd) {
-  case 'next':
-  case 'previous':
-  case 'play_pause':
-  case 'pause':
-  case 'stop':
-  case 'play':
-  case 'metadata':
-    client[cmd]()
-    break
-
-  case 'search':
-    client.search()
-    break
-
-  default:
-    help()
-    break
+function search_cmd(yargs) {
+  console.log(yargs.argv)
 }
+console.log(argv)
